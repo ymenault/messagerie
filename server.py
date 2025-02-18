e4487a508c2f3cd6abbeac369ad5ba93948cf412
matghue <mhilaire@guardiaschool.fr>
2025-02-18 15:28:32 +0100

    ajout matrice !

--- server.py
+++ server.py
@@ -1,22 +1,19 @@
 import socket 
 import threading
 
-from cesar import encrypt
-
 IP = "127.0.0.1"
 PORT = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((IP, PORT))
 server.listen(10)
 
-clients = []
-pseudos = []
+# Matrice stockant les clients sous forme [client_socket, pseudo]
+clients_data = []
 
 def broadcast(message):
-    for client in clients:
-        client.send(bytes(message, "utf-8"))
+    for client_info in clients_data:
+        client_info[0].send(bytes(message, "utf-8"))
 
 def handle_connexion():
     while True:
@@ -24,9 +21,7 @@ def handle_connexion():
         print(f"Connection from {adress} has been established!")
 
         pseudo = client.recv(1024).decode("utf-8")
-
-        clients.append(client)
-        pseudos.append(pseudo)
+        clients_data.append([client, pseudo])
 
         print(f"{pseudo} has joined the chat!")
 
@@ -38,13 +33,16 @@ def handle_client(client, pseudo):
         try:
             message = client.recv(1024).decode("utf-8")
             broadcast(f"{pseudo} : {message}")
-
         except:
-            index = clients.index(client)
-            clients.remove(client)
+            remove_client(client)
+            break
+
+def remove_client(client):
+    for client_info in clients_data:
+        if client_info[0] == client:
+            clients_data.remove(client_info)
             client.close()
-            pseudo = pseudos[index]
-            pseudos.remove(pseudo)
+            print(f"{client_info[1]} has left the chat! \n")
             break
 
 print("Server is listening...")
